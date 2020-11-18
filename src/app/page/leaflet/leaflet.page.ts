47b8b99526464c7e44229547c846141f7669378d
703Pvrtcx <kasaokapapkies@gmail.com>
2020-11-18 22:08:43 +0200

    Added Leaflet page using Pretoria  coodinates

--- src/app/page/leaflet/leaflet.page.ts
+++ src/app/page/leaflet/leaflet.page.ts
@@ -12,7 +12,7 @@ export class LeafletPage implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    var mymap = L.map('mapid').setView([-25.7446,28.18783], 13);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
